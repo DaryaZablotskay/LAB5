77c2a2909689625b64943ebca4baf8a554d078e9
DashaZablotskaya <dasaz659@gmail.com>
2021-12-07 17:32:51 +0300

    created GraphicsDisplay class

---
+++ src/bsu/rfe/java/group6/lab5/Zablotskaya/varA12/Main.java
@@ -0,0 +1,13 @@
+package bsu.rfe.java.group6.lab5.Zablotskaya.varA12;
+
+
+public class Main{
+    public static void main(String[] args) {
+        MainFrame frame = new MainFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+}
+
+
